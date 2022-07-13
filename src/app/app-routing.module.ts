9019fc29350d3e521f9217571b35494bca7b3539
MedBouhdida <bouhdidamohamed22@gmail.com>
2022-07-13 19:35:33 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AjouterAdminComponent } from './ajouter-admin/ajouter-admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: 'ajouterAdmin', component: AjouterAdminComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
