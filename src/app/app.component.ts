1f136de01e9ed2d17cc749941c67f2f94eb7471e
MedBouhdida <bouhdidamohamed22@gmail.com>
2022-07-20 20:05:00 +0100

    Revert "titleTest"
    This reverts commit 72ef570bccb7400d9e6f1735fbd871da58ff60a8.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { BrowserModule, Title } from '@angular/platform-browser';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'angularTitle';
-
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private titleService: Title,
-    private router: Router
-  ) { }
-  setDocTitle(title: string) {
-    console.log('current title:::::' + this.titleService.getTitle());
-    this.titleService.setTitle(title);
-  }
-  ngOnInit() {
-    const appTitle = this.titleService.getTitle();
-    this.router
-      .events.pipe(
-        filter(event => event instanceof NavigationEnd),
-        map(() => {
-          let child = this.activatedRoute.firstChild;
-          while (child.firstChild) {
-            child = child.firstChild;
-          }
-          if (child.snapshot.data['title']) {
-            return child.snapshot.data['title'];
-          }
-          return appTitle;
-        })
-      ).subscribe((ttl: string) => {
-        this.titleService.setTitle(ttl);
-      });
-  }
+export class AppComponent {
+  title = 'Admin';
 }
